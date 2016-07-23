c02708914e532358792c1cbe1e4d076a1ea7e97e
Shawn Garner <shawndgarner@gmail.com>
2016-07-22 22:54:51 -0500

    added ReflexVacuumAgentSpec

---
+++ core/src/test/scala/aima/core/environment/vacuum/ReflexVacuumAgentSpec.scala
@@ -0,0 +1,33 @@
+package aima.core.environment.vacuum
+
+import org.specs2.mutable.Specification
+import org.specs2.specification.Scope
+
+/**
+  * @author Shawn Garner
+  */
+class ReflexVacuumAgentSpec extends Specification {
+
+  "should move right if location A" in new context {
+    agent.agentFunction.apply(LocationAPercept) must beLike {
+      case RightMoveAction => ok
+    }
+  }
+
+  "should move left if location B" in new context {
+    agent.agentFunction.apply(LocationBPercept) must beLike {
+      case LeftMoveAction => ok
+    }
+  }
+
+  "should suck if dirty" in new context {
+    agent.agentFunction.apply(DirtyPercept) must beLike {
+      case Suck => ok
+    }
+  }
+
+  trait context extends Scope {
+    val agent = new ReflexVacuumAgent
+
+  }
+}
