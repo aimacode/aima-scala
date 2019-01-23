79e5ddac4192cf9921bb3369484c00221290219f
Shawn Garner <shawndgarner@gmail.com>
2019-01-23 10:26:32 -0600

    prove basic schedule can not produce a negative temperature using scala check property (lawful) base testing for all valid timesteps in the timestep domain

--- core/src/test/scala/aima/core/search/local/SimulatedAnnealingSearchSpec.scala
+++ core/src/test/scala/aima/core/search/local/SimulatedAnnealingSearchSpec.scala
@@ -2,6 +2,8 @@ package aima.core.search.local
 
 import aima.core.search.local.SimulatedAnnealingSearch.{OverTimeStepLimit, Temperature, TemperatureResult}
 import aima.core.search.local.time.TimeStep
+import org.scalacheck.{Arbitrary, Gen}
+import org.specs2.ScalaCheck
 import org.specs2.mutable.Specification
 
 import scala.util.{Success, Try}
@@ -9,7 +11,7 @@ import scala.util.{Success, Try}
 /**
   * @author Shawn Garner
   */
-class SimulatedAnnealingSearchSpec extends Specification {
+class SimulatedAnnealingSearchSpec extends Specification with ScalaCheck {
 
   "BasicSchedule" >> {
     import aima.core.search.local.SimulatedAnnealingSearch.BasicSchedule.schedule
@@ -38,6 +40,20 @@ class SimulatedAnnealingSearchSpec extends Specification {
     "over limit check" in {
       increment(TimeStep.start, 99).map(schedule(_)) must beSuccessfulTry[TemperatureResult](OverTimeStepLimit)
     }
+
+    implicit val arbTimeStep: Arbitrary[TimeStep] = Arbitrary {
+      for {
+        numSteps <- Gen.choose[Int](0, 98)
+      } yield increment(TimeStep.start, numSteps).get
+    }
+
+    "must not create negative temperature" >> prop { ts: TimeStep =>
+      schedule(ts) match {
+        case Temperature(t) => t must be_>(0.00d)
+        case other          => ko(other.toString)
+      }
+    }
+
   }
 
 }
