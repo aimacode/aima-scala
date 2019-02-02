20f4b6a8ae984fbdf9ac60bf8f79559afcc9c095
Aditya Lahiri <aditya.lahiri2009@gmail.com>
2019-02-02 22:54:44 +0530

    Game trait created.

---
+++ core/src/main/scala/aima/core/search/adversarial/Game.scala
@@ -0,0 +1,16 @@
+package aima.core.search.adversarial
+
+import aima.core.agent.Action
+
+final case class UtilityValue[T: Numeric](value: T) extends AnyVal
+
+/**
+  * @author Aditya Lahiri
+  */
+trait Game[Player, State, Action] {
+  def initialState: State
+  def getPlayer(state: State): Player 
+  def getActions(state: State): List[Action]
+  def result(state: State, action: Action): State
+  def getUtility(state: State, player: Player): UtilityValue
+}
