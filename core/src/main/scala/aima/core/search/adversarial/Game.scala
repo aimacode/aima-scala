b80af13fc9cf7c44e929d172337120cc3e2f49b2
Aditya Lahiri <aditya.lahiri2009@gmail.com>
2019-02-10 14:15:56 +0530

    parameter error resolved

--- core/src/main/scala/aima/core/search/adversarial/Game.scala
+++ core/src/main/scala/aima/core/search/adversarial/Game.scala
@@ -2,7 +2,7 @@ package aima.core.search.adversarial
 
 import aima.core.agent.Action
 
-final case class UtilityValue[T: Numeric](value: T) extends AnyVal
+final case class UtilityValue[T](value: Numeric[T]) extends AnyVal
 
 /**
   * @author Aditya Lahiri
@@ -12,5 +12,5 @@ trait Game[Player, State, Action] {
   def getPlayer(state: State): Player 
   def getActions(state: State): List[Action]
   def result(state: State, action: Action): State
-  def getUtility(state: State, player: Player): UtilityValue
+  def getUtility(state: State, player: Player): UtilityValue[Double]
 }
