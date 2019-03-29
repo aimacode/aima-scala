b0480881bcc4367a19f492aae104c32dadac1517
Aditya Lahiri <aditya.lahiri2009@gmail.com>
2019-03-29 22:46:18 +0530

    added missing isTerminalState function

--- core/src/main/scala/aima/core/search/adversarial/Game.scala
+++ core/src/main/scala/aima/core/search/adversarial/Game.scala
@@ -1,16 +1,18 @@
 package aima.core.search.adversarial
 
-import aima.core.agent.Action
+import aima.core.agent.{Action, NoAction}
 
 final case class UtilityValue(value: Double) extends AnyVal
 
 /**
   * @author Aditya Lahiri
+  * @author Shawn Garner
   */
 trait Game[Player, State, Action] {
   def initialState: State
   def getPlayer(state: State): Player 
   def getActions(state: State): List[Action]
   def result(state: State, action: Action): State
+  def isTerminalState(state: State): Boolean
   def getUtility(state: State, player: Player): UtilityValue
 }
