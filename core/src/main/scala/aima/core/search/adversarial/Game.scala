d1dd9ec644c41713052f76d5bf911c7d4a47df51
Aditya Lahiri <aditya.lahiri2009@gmail.com>
2019-04-14 13:53:38 +0530

    2nd, 3rd, 4th, 5th, 6th requested changes made, errors resolved
    Integrated all requested changes and remedied handful of syntactical and logical errors.

--- core/src/main/scala/aima/core/search/adversarial/Game.scala
+++ core/src/main/scala/aima/core/search/adversarial/Game.scala
@@ -1,6 +1,10 @@
 package aima.core.search.adversarial
 
-final case class UtilityValue(value: Double) extends AnyVal
+final case class UtilityValue(value: Double) extends AnyVal {
+  def <(that: UtilityValue) = if (this.value < that.value) true else false
+  def >(that: UtilityValue) = if (this.value > that.value) true else false
+
+}
 
 /**
   * @author Aditya Lahiri
@@ -8,7 +12,7 @@ final case class UtilityValue(value: Double) extends AnyVal
   */
 trait Game[Player, State, Action] {
   def initialState: State
-  def getPlayer(state: State): Player 
+  def getPlayer(state: State): Player
   def getActions(state: State): List[Action]
   def result(state: State, action: Action): State
   def isTerminalState(state: State): Boolean
--- core/src/main/scala/aima/core/search/adversarial/MinimaxSearch.scala
+++ core/src/main/scala/aima/core/search/adversarial/MinimaxSearch.scala
@@ -1,43 +1,42 @@
 package aima.core.search.adversarial
 
-import aima.core.search.adversarial.{Game, UtilityValue}
+import scala.annotation.tailrec
 
 /**
   * @author Aditya Lahiri
   * @author Shawn Garner
   */
-
 object MinimaxDecision {
-    def minMaxDecision[S, A, P](g: Game[S][A][P]): S, P => A = {
-        (s: S, p: P) => maxMinValue(g.getActions(s), p)
-            
-        def maxMinValue(actions: List[Action], p: P): Action = actions match {
-            case Nil => NoAction
-            case a :: Nil => a
-            case a1 :: a2 :: rest => maxMinValue( (if (g.minValue(a1, p) > g.minValue(a2, p)) a1 else a2) :: rest )
-        }
-
-        def minMaxValue(actions: List[Action], p: P): Action = actions match {
-            case Nil => NoAction
-            case List(a: Action) => a
-            case a1 :: a2 :: rest => minMaxValue( (if (g.maxValue(a1, p) < g.maxValue(a2, p)) a1 else a2) :: rest )
-        }
-
-        def maxValue(s: S, p: P), : UtilityValue = {
-            if (g.isTerminalState(s))
-                g.getUtility(s, p)
-
-            else
-                maxMinValue(g.getActions(s), p)
-        }
-
-        def minValue(s: S, p: P): UtilityValue = {
-            if (g.isTerminalState(s))
-                g.getUtility(s, p)
-
-
-            else
-                minMaxValue(g.getActions(s), p)
-        }
+  def minMaxDecision[PLAYER, STATE, ACTION](g: Game[PLAYER, STATE, ACTION],
+                                            noAction: ACTION): (STATE, PLAYER) => ACTION = { (s: STATE, p: PLAYER) =>
+    @tailrec def maxMinValue(Actions: List[ACTION], p: PLAYER): ACTION = Actions match {
+      case Nil      => noAction
+      case a :: Nil => a
+      case a1 :: a2 :: rest =>
+        maxMinValue((if (minValue(g.result(s, a1), p) > minValue(g.result(s, a2), p)) a1 else a2) :: rest, p)
     }
+
+    @tailrec def minMaxValue(Actions: List[ACTION], p: PLAYER): ACTION = Actions match {
+      case Nil      => noAction
+      case a :: Nil => a
+      case a1 :: a2 :: rest =>
+        minMaxValue((if (maxValue(g.result(s, a1), p) < maxValue(g.result(s, a2), p)) a1 else a2) :: rest, p)
+    }
+
+    def maxValue(s: STATE, p: PLAYER): UtilityValue = {
+      if (g.isTerminalState(s))
+        g.getUtility(s, p)
+      else
+        minValue(g.result(s, maxMinValue(g.getActions(s), p)), p)
+    }
+
+    def minValue(s: STATE, p: PLAYER): UtilityValue = {
+      if (g.isTerminalState(s))
+        g.getUtility(s, p)
+      else
+        maxValue(g.result(s, minMaxValue(g.getActions(s), p)), p)
+    }
+
+    maxMinValue(g.getActions(s), p)
+  }
 }
