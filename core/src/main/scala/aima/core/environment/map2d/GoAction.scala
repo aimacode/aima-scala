df6c0ffe6364e4f89f413f54234c5ab0423999e2
Shawn Garner <shawndgarner@gmail.com>
2019-05-18 22:49:56 -0500

    adding map2d stuff to support writing lrta agent spec

---
+++ core/src/main/scala/aima/core/environment/map2d/GoAction.scala
@@ -0,0 +1,6 @@
+package aima.core.environment.map2d
+
+/**
+  * @author Shawn Garner
+  */
+final case class GoAction(gotTo: String) extends AnyVal
---
+++ core/src/main/scala/aima/core/environment/map2d/Map2DFunctionFactory.scala
@@ -0,0 +1,63 @@
+package aima.core.environment.map2d
+
+import aima.core.search.CostNode
+
+/**
+  * @author Shawn Garner
+  */
+object Map2DFunctionFactory {
+  import aima.core.fp.Eqv
+  import Eqv.Implicits._
+
+
+  def actions(map2D: Map2D): InState => List[GoAction] =
+    inState => map2D.locationsLinkedTo(inState.location).map(GoAction)
+
+
+  def stepCost(map2D: Map2D): (InState, GoAction, InState) => Double =
+    (s, _, sPrime) => map2D.distance(s.location, sPrime.location).map(_.value).getOrElse(Double.MaxValue)
+
+  def result(map2D: Map2D): (InState, GoAction) => InState =
+    (_, action) => InState(action.gotTo)
+
+  def goalTestPredicate(goalLocations: String*): InState => Boolean =
+    inState => goalLocations.exists(location => location === inState.location)
+
+  object StraightLineDistanceHeuristic {
+
+    def apply(map2D: Map2D, goals: String*): CostNode[InState, GoAction] => Double =
+      node => {
+
+
+        def h(state: InState): Double = {
+          val distances: List[Double] = goals.toList.flatMap { goal =>
+
+            val distance: Option[Double] = for {
+              currentPosition <- map2D.position(state.location)
+              goalPosition <- map2D.position(goal)
+            } yield distanceOf(currentPosition, goalPosition)
+
+            distance.toList
+          }
+
+          distances match {
+            case Nil => Double.MaxValue
+            case _ => distances.min
+          }
+
+        }
+
+        def distanceOf(p1: Point2D, p2: Point2D): Double = {
+          math.sqrt((p1.x - p2.x) * (p1.x - p2.x)
+            + (p1.y - p2.y) * (p1.y - p2.y))
+        }
+
+        h(node.state)
+
+      }
+
+
+
+  }
+
+}
