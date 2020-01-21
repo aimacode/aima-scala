ce12f779160e40c7284656923762941cd2f16ac1
Damien Favre <dam.favre@gmail.com>
2020-01-21 21:45:06 +0000

    abstract randomness

--- core/src/main/scala/aima/core/agent/Actuator.scala
+++ core/src/main/scala/aima/core/agent/Actuator.scala
@@ -1,9 +1,23 @@
 package aima.core.agent
 
+import aima.core.random.{DefaultRandomness, Randomness}
+
 /**
   * @author Shawn Garner
   * @author Damien Favre
   */
 trait Actuator[ENVIRONMENT, ACTION] {
   def act(action: ACTION, e: ENVIRONMENT): Option[ENVIRONMENT]
+}
+
+object UnreliableActuator {
+  def fromActuator[ENVIRONMENT, ACTION](
+      actuator: Actuator[ENVIRONMENT, ACTION]
+  )(
+      reliability: Int = 50,
+      randomness: Randomness = new DefaultRandomness {}
+  ): Actuator[ENVIRONMENT, ACTION] = new Actuator[ENVIRONMENT, ACTION] {
+    override def act(action: ACTION, e: ENVIRONMENT): Option[ENVIRONMENT] =
+      randomness.unreliably(reliability)(actuator.act(action, e)).flatten
+  }
 }
--- core/src/main/scala/aima/core/agent/Sensor.scala
+++ core/src/main/scala/aima/core/agent/Sensor.scala
@@ -1,5 +1,20 @@
 package aima.core.agent
 
+import aima.core.random.{DefaultRandomness, Randomness}
+
 trait Sensor[ENVIRONMENT, PERCEPT] {
   def perceive(e: ENVIRONMENT): Option[PERCEPT]
+}
+
+object UnreliableSensor {
+  def fromSensor[ENVIRONMENT, PERCEPT](
+      sensor: Sensor[ENVIRONMENT, PERCEPT]
+  )(
+      reliability: Int = 50,
+      randomness: Randomness = new DefaultRandomness {}
+  ): Sensor[ENVIRONMENT, PERCEPT] = new Sensor[ENVIRONMENT, PERCEPT] {
+    override def perceive(e: ENVIRONMENT): Option[PERCEPT] =
+      randomness.unreliably(reliability)(sensor.perceive(e)).flatten
+  }
+
 }
