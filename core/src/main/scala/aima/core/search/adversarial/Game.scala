e4e4b0602bed888a46d0494fc5b1864ee4e61984
Aditya Lahiri <aditya.lahiri2009@gmail.com>
2020-02-02 03:01:10 +0530

    [WIP] Minimax search function (#71)
    * Game trait created.
    
    * parameter error resolved
    
    * removed implicit parameter
    
    * added missing isTerminalState function
    
    * minimaxsearch added
    
    * 1st requested change
    
    case a :: Nil used instead to improve consistency with other cases.
    
    * 2nd, 3rd, 4th, 5th, 6th requested changes made, errors resolved
    
    Integrated all requested changes and remedied handful of syntactical and logical errors.
    
    * added test problem for minimax
    
    * test added
    
    test spec for minimax
    
    * reformatting
    
    * shanges
    
    changes
    
    * review changes
    
    * errors fixed, spec added
    
    * clean
    
    Co-authored-by: Shawn Garner <BusyByte@users.noreply.github.com>

--- core/src/main/scala/aima/core/search/adversarial/Game.scala
+++ core/src/main/scala/aima/core/search/adversarial/Game.scala
@@ -1,14 +1,27 @@
 package aima.core.search.adversarial
 
-final case class UtilityValue(value: Double) extends AnyVal
+final case class UtilityValue(value: Double) extends AnyVal {
+  def <(that: UtilityValue)  = if (this.value < that.value) true else false
+  def >(that: UtilityValue)  = if (this.value > that.value) true else false
+  def ==(that: UtilityValue) = if (this.value == that.value) true else false
+
+}
 
 /**
   * @author Aditya Lahiri
+  * @author Shawn Garner
   */
-trait Game[Player, State, Action] {
-  def initialState: State
-  def getPlayer(state: State): Player
-  def getActions(state: State): List[Action]
-  def result(state: State, action: Action): State
-  def getUtility(state: State, player: Player): UtilityValue
+trait Game[P <: Player, S <: State, A <: Action] {
+  def initialState: S
+  def getPlayer(state: S): P
+  def getActions(state: S): List[A]
+  def result(state: S, action: A): S
+  def isTerminalState(state: S): Boolean
+  def getUtility(state: S): UtilityValue
 }
+
+trait Player {}
+
+trait State {}
+
+trait Action {}
---
+++ core/src/main/scala/aima/core/search/adversarial/MinimaxSearch.scala
@@ -0,0 +1,50 @@
+package aima.core.search.adversarial
+
+import scala.annotation.tailrec
+
+/**
+  * @author Aditya Lahiri
+  * @author Shawn Garner
+  */
+object MinimaxDecision {
+  def minMaxDecision[PLAYER <: Player, STATE <: State, ACTION <: Action](
+      g: Game[PLAYER, STATE, ACTION],
+      noAction: ACTION
+  ): (STATE) => ACTION = { (state: STATE) =>
+    @tailrec def maxMinValue(Actions: List[ACTION]): ACTION = Actions match {
+      case Nil                   => noAction
+      case singularAction :: Nil => singularAction
+      case action1 :: action2 :: rest =>
+        maxMinValue(
+          (if (minValue(g.result(state, action1)) > minValue(g.result(state, action2))) action1
+           else action2) :: rest
+        )
+    }
+
+    @tailrec def minMaxValue(Actions: List[ACTION]): ACTION = Actions match {
+      case Nil                   => noAction
+      case singularAction :: Nil => singularAction
+      case action1 :: action2 :: rest =>
+        minMaxValue(
+          (if (maxValue(g.result(state, action1)) < maxValue(g.result(state, action2))) action1
+           else action2) :: rest
+        )
+    }
+
+    def maxValue(state: STATE): UtilityValue = {
+      if (g.isTerminalState(state))
+        g.getUtility(state)
+      else
+        minValue(g.result(state, maxMinValue(g.getActions(state))))
+    }
+
+    def minValue(state: STATE): UtilityValue = {
+      if (g.isTerminalState(state))
+        g.getUtility(state)
+      else
+        maxValue(g.result(state, minMaxValue(g.getActions(state))))
+    }
+
+    maxMinValue(g.getActions(g.initialState))
+  }
+}
