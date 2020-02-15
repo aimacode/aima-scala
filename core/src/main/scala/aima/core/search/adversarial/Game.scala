5f9ec3281c11057313fb0762e1f215d5bfd48670
Shawn Garner <shawndgarner@gmail.com>
2020-02-15 13:21:29 -0600

    refectored Game and Two Player game a bit

--- core/src/main/scala/aima/core/search/adversarial/Game.scala
+++ core/src/main/scala/aima/core/search/adversarial/Game.scala
@@ -1,27 +1,36 @@
 package aima.core.search.adversarial
 
-final case class UtilityValue(value: Double) extends AnyVal {
-  def <(that: UtilityValue)  = if (this.value < that.value) true else false
-  def >(that: UtilityValue)  = if (this.value > that.value) true else false
-  def ==(that: UtilityValue) = if (this.value == that.value) true else false
+final case class UtilityValue(value: Double) extends AnyVal
 
+object UtilityValue {
+  implicit def utilityValueOrdering(implicit dOrdering: Ordering[Double]): Ordering[UtilityValue] =
+    new Ordering[UtilityValue] {
+      override def compare(x: UtilityValue, y: UtilityValue): Int = dOrdering.compare(x.value, y.value)
+    }
+  implicit class UtilityValueOps(value: UtilityValue) {
+    def <(other: UtilityValue)(implicit o: Ordering[UtilityValue]): Boolean = {
+      o.compare(value, other) < 0
+    }
+
+    def >(other: UtilityValue)(implicit o: Ordering[UtilityValue]): Boolean = {
+      o.compare(value, other) > 0
+    }
+
+    def ==(other: UtilityValue)(implicit o: Ordering[UtilityValue]): Boolean = {
+      o.compare(value, other) == 0
+    }
+  }
 }
 
 /**
   * @author Aditya Lahiri
   * @author Shawn Garner
   */
-trait Game[P <: Player, S <: State, A <: Action] {
-  def initialState: S
-  def getPlayer(state: S): P
-  def getActions(state: S): List[A]
-  def result(state: S, action: A): S
-  def isTerminalState(state: S): Boolean
-  def getUtility(state: S): UtilityValue
+trait Game[PLAYER, STATE, ACTION] {
+  def initialState: STATE
+  def getPlayer(state: STATE): PLAYER
+  def getActions(state: STATE): List[ACTION]
+  def result(state: STATE, action: ACTION): STATE
+  def isTerminalState(state: STATE): Boolean
+  def getUtility(state: STATE): UtilityValue
 }
-
-trait Player {}
-
-trait State {}
-
-trait Action {}
